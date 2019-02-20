75654f4ef47b1665a505953b0d38d2d16ffa614d
Edouard Chatzopoulos <edouard.chatzopoulos@student.uclouvain.be>
2019-02-20 14:11:23 +0100

    correction 2

---
+++ Troubadour/desktop/src/com/troubadour/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.troubadour.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.troubadour.game.Troubadour;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Troubadour(), config);
+	}
+}

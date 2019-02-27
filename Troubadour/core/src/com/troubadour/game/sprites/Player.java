3355644782d7b0a77207fb8435cd509a6bff2b5e
Edouard Chatzopoulos <edouard.chatzopoulos@student.uclouvain.be>
2019-02-27 14:15:20 +0100

    sprite animation

--- Troubadour/core/src/com/troubadour/game/sprites/Player.java
+++ Troubadour/core/src/com/troubadour/game/sprites/Player.java
@@ -2,6 +2,7 @@ package com.troubadour.game.sprites;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector3;
 import com.troubadour.game.Troubadour;
 
@@ -11,19 +12,23 @@ public class Player {
     private static final float MOVEMENT = 80;
     private Vector3 position;
     private Vector3 velocity;
+    private Texture texture;
 
     private Texture player;
+    private Animation playerAnimation;
 
     public Player(int x, int y){
         position = new Vector3(x, y, 0);
         velocity = new Vector3(0, 0, 0);
-        player = new Texture("player.png");
+        texture = new Texture("troubadouranimation.png");
+        playerAnimation = new Animation(new TextureRegion(texture), 3, 0.2f);
+
 
 
     }
 
     public void update(float dt){
-
+        playerAnimation.update(dt);
         velocity.scl(dt);
         position.add(0, MOVEMENT*dt, 0);
         velocity.scl(1/dt);
@@ -41,11 +46,14 @@ public class Player {
     }
 
 
-    public Texture getTexture() {
-        return player;
+    public TextureRegion getTexture() {
+        return playerAnimation.getFrame();
     }
 
     public void move(){
         position.x = (Gdx.input.getX()/2)-(PLAYER_WIDTH/2);
+    }
+    public void dispose(){
+        texture.dispose();
     }
 }

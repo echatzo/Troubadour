c4056174cb066e8d9dd7a45f47e3bf5a628897d6
Edouard Chatzopoulos <edouard.chatzopoulos@student.uclouvain.be>
2019-03-02 23:53:39 +0100

    Adding mob abstract class

---
+++ Troubadour/core/src/com/troubadour/game/sprites/Mob.java
@@ -0,0 +1,34 @@
+package com.troubadour.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+public abstract class Mob {
+    private Vector3 position;
+    private Vector3 velocity;
+    private Texture texture;
+    private Rectangle bounds;
+    private Animation mobAnimation;
+
+    private Mob(int x, int y){
+        position = new Vector3(x, y, 0);
+        velocity = new Vector3(0, 0, 0);
+    }
+
+    public abstract void update(float dt);
+
+    public abstract Vector3 getPosition();
+
+    public abstract TextureRegion getTexture();
+
+    public abstract void dispose();
+
+    public abstract Rectangle getBounds();
+
+
+
+
+
+}

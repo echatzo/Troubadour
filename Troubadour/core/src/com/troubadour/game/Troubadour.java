05647638a25087f832a75020d1446ff0d06149c3
nicolascayphas <nicolas.cayphas@yahoo.com>
2019-03-03 14:02:30 +0100

    ajout music dans state troubadour

--- Troubadour/core/src/com/troubadour/game/Troubadour.java
+++ Troubadour/core/src/com/troubadour/game/Troubadour.java
@@ -2,6 +2,7 @@ package com.troubadour.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.troubadour.game.states.GameStateManager;
@@ -18,10 +19,18 @@ public class Troubadour extends ApplicationAdapter {
 	private GameStateManager gsm;
 	private SpriteBatch batch;
 
+	private Music music;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
+		//music debut
+        music = Gdx.audio.newMusic((Gdx.files.internal("music.mp3")));
+        music.setLooping(true);
+        music.setVolume(0.1f);
+        music.play();
+        //music fin
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		gsm.push(new MenuState(gsm));
 	}
@@ -32,4 +41,10 @@ public class Troubadour extends ApplicationAdapter {
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
 	}
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        music.dispose();
+    }
 }
